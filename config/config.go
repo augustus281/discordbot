ab88d10eaf35449f39667c197ed6e0bfcd45c702
augustus <huynd@inspirelab.vn>
2024-08-25 09:05:48 +0700

    DEV

---
+++ config/config.go
@@ -0,0 +1,16 @@
+package config
+
+var (
+	Token     string
+	BotPrefix string
+	config    *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+}
+
+func ReadConfig() error {
+	
+}
